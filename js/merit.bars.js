041c53c2b700c332da4e074f24d84039d7735bf4
Sébastien Arnold <seba-1511@2754171.no-reply.drupal.org>
2013-12-18 17:54:18 +0100

    Added automatically shading the border for progress bars.

--- js/merit.bars.js
+++ js/merit.bars.js
@@ -1,7 +1,17 @@
 var bars = function() {
     'use strict';
 
+    var shadeColor;
     var testThreshold = 2;
+
+    shadeColor = function(color, percent) {
+        var num = parseInt(color.slice(1), 16),
+            amt = Math.round(2.55 * percent),
+            R = (num >> 16) + amt,
+            G = (num >> 8 & 0x00FF) + amt,
+            B = (num & 0x0000FF) + amt;
+        return "#" + (0x1000000 + (R < 255 ? R < 1 ? 0 : R : 255) * 0x10000 + (G < 255 ? G < 1 ? 0 : G : 255) * 0x100 + (B < 255 ? B < 1 ? 0 : B : 255)).toString(16).slice(1);
+    };
 
     return {
 
@@ -81,7 +91,7 @@ var bars = function() {
             bar = jQuery(bar);
             if (color) {
                 bar.find('.ui-progressbar-value').css('background', color);
-                bar.find('.ui-widget-header').css('border', 'solid ' + color + ' 3px');
+                bar.find('.ui-widget-header').css('border', 'solid ' + shadeColor(color, 30) + ' 3px');
             }
 
             bar.find('.ui-progressbar-value').css('margin', '0px');
